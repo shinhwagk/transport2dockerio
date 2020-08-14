31715bbadbad20693cd9d57ac2586e8e0c79ffa4
shinhwagk <shanghai_sl@hotmail.com>
2020-08-14 17:22:31 +0800

    test

--- proxy.sh
+++ proxy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+RenameToDockerIo(){
+    echo "shinhwagk/quayio_${1}_${2}:${3}"
+}
 
-
-quayio
+# Porcess quay.io
+ls quay.io | while read repo; do
+  ls "quay.io/$repo" | while read image; do
+    cat "quay.io/$repo/$image" | while read tag; do
+      echo "docker pull quay.io/$repo/$image:${tag}"
+      dockerioImage=`RenameToDockerIo $repo $image $tag`
+      docker push ${dockerioImage}
+    done
+  done
+done
