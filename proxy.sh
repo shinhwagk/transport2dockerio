7dd903ecc3a1c179873b1e2cf01d7a96df0b9400
shinhwagk <shanghai_sl@hotmail.com>
2020-08-14 17:29:07 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -5,12 +5,24 @@ RenameToDockerIo(){
 }
 
 # Porcess quay.io
+echo "start process quay.io"
 ls quay.io | while read repo; do
+  echo "  start process quay.io repo: ${repo}"
   ls "quay.io/$repo" | while read image; do
+    echo "    start process quay.io repo: ${repo} image: ${image}"
     cat "quay.io/$repo/$image" | while read tag; do
-      echo "docker pull quay.io/$repo/$image:${tag}"
+      echo "      start process quay.io repo: ${repo} image: ${image} tag: ${tag}"
+      echo "        start pull quay.io/$repo/$image:${tag}"
+      docker pull quay.io/$repo/$image:${tag}
+      echo "        success pull quay.io/$repo/$image:${tag}"
+
+      echo "        start rename quay.io/$repo/$image:${tag}"
       dockerioImage=`RenameToDockerIo $repo $image $tag`
+      echo "        success rename quay.io/$repo/$image:${tag} -> ${dockerioImage}"
+
+      echo "        start push ${dockerioImage}"
       docker push ${dockerioImage}
+      echo "        success push ${dockerioImage}"
     done
   done
 done
